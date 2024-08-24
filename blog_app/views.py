fbd26bda7f73e6fbd7014797165cbc2ad3f497e3
v <v@mail.ru>
2024-08-24 14:02:08 +0500

    Создание первого шаблона

--- blog_app/views.py
+++ blog_app/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render, HttpResponse
 
 # Index - функция, которая будет отдавать HttpResponse с текстом "Привет из Django!"
 def index(request):
-    return HttpResponse("Привет из Django!")
+    return render(request, 'index.html')
+
+

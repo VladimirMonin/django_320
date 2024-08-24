41d41dce4e9f8eacdff0865b3ad13ae9c7e7eadf
v <v@mail.ru>
2024-08-24 17:58:38 +0500

    переменные шаблонов

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render, HttpResponse
 
+class Data:
+    def __init__(self, name):
+        self.name = name
+        
+    def method(self):
+        return 'Вызов метода класса Data'
+
+
 # Index - функция, которая будет отдавать HttpResponse с текстом "Привет из Django!"
 def index(request):
-    context = {'name': 'Zigmund'}
+    
+    data_instance = Data('Гендальф')
+    
+    context = {'some_str': 'Тест JS переменной',
+               'some_int': 88,
+               'some_list': ['Один', 1],
+               'some_dict': {'fruit': 'apple'},
+               'data': data_instance}
     return render(request, 'index.html', context=context)
 
 

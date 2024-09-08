ca2ec9f10bce8791ec648fa71e4cabdffcc6ffc9
v <v@mail.ru>
2024-09-08 13:08:48 +0500

    lesson_47: наследование index от base шаблона

--- blog_app/views.py
+++ blog_app/views.py
@@ -22,11 +22,9 @@ def post_by_slug(request, post_slug) -> HttpResponse:
     Проходим словарей dataset и ищем совпадение по слагу    Если пост не, возвращаем 404.
     В противном случае возвращаем детальную информацию о посте.
     """
-    # Ищем пост по слагу
-    post = [post post in dataset if post['slug'] post_slug]
-    # Если пост не найден, возвращаем 404
+    post = [post for post in dataset if post['slug'] == post_slug]
     if not post:
+        # 404 - Пост не найден
         return HttpResponse('404 - Пост не найден', status=404)
-    # Возвращаем детальную информацию о посте, если он найден
     else:
         return render(request, 'blog_app/post_detail.html', context=post[0], status=200)

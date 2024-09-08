db97195893783f3804a063a1f469818f9f5fb15b
v <v@mail.ru>
2024-09-08 12:41:34 +0500

    lesson_47: Базовый шаблон

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,19 +1,32 @@
 from django.shortcuts import render, HttpResponse
 from .dataset import dataset
 
-# Index - функция, которая будет отдавать HttpResponse с текстом "Привет из Django!"
-def index(request):
+
+def index(request) -> HttpResponse:
+    """
+    Функция представления для отображения главной страницы блога.
+    Параметры:
+    request: объект запроса Django, который содержит информацию оем запросе.
+
+   вращает:
+    HttpResponse: HTTP-ответ с отрендеренной страницей 'index.html' 
+    и контекст, содержащим список постов блога.
+    """
     context = {
         'posts': dataset
     }
     return render(request, 'blog_app/index.html', context=context)
 
-
-def post_by_slug(request, post_slug):
-    # Проходим список словарей dataset и ищем совпадение по слагу
-    post = [post for post in dataset if post['slug'] == post_slug]
+def post_by_slug(request, post_slug) -> HttpResponse:
+    """
+    Проходим словарей dataset и ищем совпадение по слагу    Если пост не, возвращаем 404.
+    В противном случае возвращаем детальную информацию о посте.
+    """
+    # Ищем пост по слагу
+    post = [post post in dataset if post['slug'] post_slug]
+    # Если пост не найден, возвращаем 404
     if not post:
-        # 404 - Пост не найден
         return HttpResponse('404 - Пост не найден', status=404)
+    # Возвращаем детальную информацию о посте, если он найден
     else:
         return render(request, 'blog_app/post_detail.html', context=post[0], status=200)

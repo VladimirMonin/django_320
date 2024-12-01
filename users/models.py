ae04e350dccc48bf1b5536130e624294bf816e23
Vladimir <v@v.ru>
2024-12-01 13:25:07 +0500

    tg_bot: добавил телеграм бота

--- users/models.py
+++ users/models.py
@@ -1,12 +1,14 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-
+import uuid
 class User(AbstractUser):
     email = models.EmailField(unique=True, verbose_name='Email')
     photo = models.ImageField(upload_to='users/images/%Y/%m/%d/', blank=True, null=True, verbose_name='Фотография')
     date_birth = models.DateTimeField(blank=True, null=True, verbose_name='Дата рождения')
     github_id = models.CharField(max_length=255, blank=True, null=True)
     vk_id = models.CharField(max_length=255, blank=True, null=True)
+    tg_chat_id = models.CharField(max_length=255, blank=True, null=True, verbose_name='Telegram Chat ID')
+    uuid = models.UUIDField(default=uuid.uuid4, blank=True, null=True)
 
     def __str__(self):
         return f'self.username'
